029190ca624441aa2ddbd0eafec1028238563bd8
UJONE <59907964+toot09@users.noreply.github.com>
2023-04-04 15:31:22 +0900

    Add files via upload

---
+++ gradio_gpt.py
@@ -0,0 +1,33 @@
+import gradio as gr
+import requests
+import os
+import openai
+import logging
+from gradio import components
+
+openai.api_key = "openai api-key"
+
+def call_gpt_api(prompt):
+    logging.info("호출")
+    try:
+        logging.info("여긴 들어옴")
+        completion = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "user", "content": "Hello!"}
+            ]
+        )
+        print(completion.choices[0].message)
+        return completion.choices[0].message.content
+    
+    except Exception as e:
+        logging.error(e)
+        return "Error: API call failed"
+
+
+input_text = components.Textbox(lines=5, label="Input prompt")
+output_text = components.Textbox(label="GPT-generated text")
+interface = gr.Interface(fn=call_gpt_api, inputs=input_text, outputs=output_text)
+
+app = interface.launch()
+print("App is running on port:", app.port)
